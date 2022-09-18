1599b72908ccd6d00dc83585253fb782ecb16b97
jfernandesdev <jfernandes.dev@gmail.com>
2022-09-18 10:26:41 -0300

    FEAT: routes and navigability

---
+++ mobile/src/screens/Game/index.tsx
@@ -0,0 +1,56 @@
+import { View, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
+
+import { GameParams } from '../../@types/navigation';
+
+import { Background } from '../../components/Background';
+import { Heading } from '../../components/Heading';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+
+import { THEME } from '../../theme';
+import { styles } from './styles';
+
+export function Game(){
+  const navigation = useNavigation();
+  const route = useRoute();
+  const game = route.params as GameParams;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo 
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image 
+            source={logoImg}
+            style={styles.logo}
+          />
+
+          <View style={styles.right} />
+        </View>
+
+        <Image 
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+
+      </SafeAreaView>
+    </Background>
+  );
+}
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
-import { View, Image, FlatList } from 'react-native';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { Heading } from '../../components/Heading';
+import { Background } from '../../components/Background';
 import { GameCard, GameCardProps } from '../../components/GameCard';
 
 import { api } from '../../lib/api';
@@ -12,6 +15,11 @@ import { styles } from './styles';
 
 export function Home(){
   const [games, setGames] = useState<GameCardProps[]>([])
+  const navigation = useNavigation()
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl })
+  }
 
   useEffect(() => {
     async function loadGames() {
@@ -23,27 +31,32 @@ export function Home(){
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Image 
-        source={logoImg}
-        style={styles.logo}
-      />
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image 
+          source={logoImg}
+          style={styles.logo}
+        />
 
-      <Heading 
-        title="Encontre seu duo!"
-        subtitle="Selecione o game que deseja jogar..."
-      />
+        <Heading 
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."
+        />
 
-      <FlatList 
-        data={games}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => (
-          <GameCard data={item} />
-        )}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={styles.contentList}
-      />
-    </View>
+        <FlatList 
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+            <GameCard 
+              data={item} 
+              onPress={() => handleOpenGame(item)}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView>
+    </Background>
   );
 }
