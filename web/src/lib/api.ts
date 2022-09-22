15f3b068aedb7f7e7b912ba938758982c7eba454
Jeferson Fernandes <jfernandes.dev@gmail.com>
2022-09-22 10:31:20 -0300

    Update api.ts

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   // baseURL: 'http://localhost:6543',
-  baseURL: 'https://api-esports-jfernandesdev.onrender.com',
+  baseURL: 'https://api-esports-jfernandesdev.herokuapp.com/',
 })
